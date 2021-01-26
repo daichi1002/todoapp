6f203a5269196332f41876ec3cdc09b4aae35ce8
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-26 17:46:31 +0900

    Update index.vue

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -3,10 +3,10 @@
     <!-- 新規作成部分 -->
     <div class="row">
       <div class="col s10 m11">
-        <input class="form-control" placeholder="Add your task!!">
+        <input v-model="newTask" class="form-control" placeholder="Add your task!!">
       </div>
       <div class="col s2 m1">
-        <div class="btn-floating waves-effect waves-light red">
+        <div v-on:click="createTask" class="btn-floating waves-effect waves-light red">
           <i class="material-icons">add</i>
         </div>
       </div>
@@ -64,6 +64,15 @@
       displayFinishedTasks: function() {
         document.querySelector('#finished-tasks').classList.toggle('display_none');
       },
+      createTask: function() {
+        if (!this.newTask) return;
+
+        axios.post('/api/tasks', { task: { name: this.newTask } }).then((response) => {
+          this.tasks.unshift(response.data.task);
+        }, (error) => {
+          console.log(error);
+        });
+      }
     }
   }
 </script>
