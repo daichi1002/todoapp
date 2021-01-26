a232353899280861f736121b5f3e5f483fbc2500
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-26 16:55:52 +0900

    Update index.vue

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
-    <p>Index</p>
+    <!-- 新規作成部分 -->
+    <div class="row">
+      <div class="col s10 m11">
+        <input class="form-control" placeholder="Add your task!!">
+      </div>
+      <div class="col s2 m1">
+        <div class="btn-floating waves-effect waves-light red">
+          <i class="material-icons">add</i>
+        </div>
+      </div>
+    </div>
+    <!-- リスト表示部分 -->
+    <div>
+      <ul class="collection">
+        <li id="row_task_1" class="collection-item">
+          <input type="checkbox" id="task_1" />
+          <span for="task_1">Sample Task</span>
+        </li>
+        <li id="row_task_2" class="collection-item">
+          <input type="checkbox" id="task_2" />
+          <span for="task_2">Sample Task</span>
+        </li>
+        <li id="row_task_3" class="collection-item">
+          <input type="checkbox" id="task_3" />
+          <span for="task_3">Sample Task</span>
+        </li>
+      </ul>
+    </div>
+    <!-- 完了済みタスク表示ボタン -->
+    <div class="btn">Display finished tasks</div>
+    <!-- 完了済みタスク一覧 -->
+    <div id="finished-tasks" class="display_none">
+      <ul class="collection">
+        <li id="row_task_4" class="collection-item">
+          <input type="checkbox" id="'task_4" checked="checked" />
+          <span v-bind:for="task_4" class="line-through">Done Task</span>
+        </li>
+        <li id="row_task_5" class="collection-item">
+          <input type="checkbox" id="'task_5" checked="checked" />
+          <span v-bind:for="task_5" class="line-through">Done Task</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
